5dd76f9f352c96bfc29b84d9429bc3921a062e83
geebzter <geebzter@gmail.com>
2021-02-05 10:53:58 -0500

    Second version: Filters album by optional start and end year

--- recommender/forms.py
+++ recommender/forms.py
@@ -2,3 +2,5 @@ from django import forms
 
 class SearchForm(forms.Form):
     artist = forms.CharField(widget=forms.TextInput(attrs={'size': '50'}))
+    from_year = forms.IntegerField(required=False)
+    to_year = forms.IntegerField(required=False)
--- recommender/views.py
+++ recommender/views.py
@@ -6,8 +6,14 @@ from .forms import *
 from django.views.decorators.http import require_POST, require_GET
 
 
-def find_albums(artist):
-   return list(Musicdata.objects.filter(artists__contains = artist).order_by('-popularity').values('id','name'))
+def find_albums(artist, from_year = None, to_year = None):
+    query = Musicdata.objects.filter(artists__contains = artist)
+    if from_year is not None:
+        query = query.filter(year__gte = from_year)
+    if to_year is not None:
+        query = query.filter(year__lte = to_year)
+    return list(query.order_by('-popularity').values('id','name','year'))
+    
 
 @require_POST
 def searchform_post(request):
@@ -16,8 +22,15 @@ def searchform_post(request):
     # check whether it's valid:
     if form.is_valid():
         # process the data in form.cleaned_data as required
+        from_year = None if form.cleaned_data['from_year'] == None else int(form.cleaned_data['from_year'])
+        to_year = None if form.cleaned_data['to_year'] == None else int(form.cleaned_data['to_year'])
+
         return JsonResponse( {
-            'albums' : find_albums(form.cleaned_data['artist'])
+            'albums' : find_albums(
+                form.cleaned_data['artist'],
+                from_year,
+                to_year
+            )
         })
     else:
         raise Http404('Something went wrong')
@@ -26,5 +39,6 @@ def searchform_post(request):
 @require_GET
 def searchform_get(request):
     form = SearchForm()
+
     return render(request, 'recommender/searchform.html', {'form': form})
 
