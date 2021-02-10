e78ab1c61507d5e3e677aed59fe68beaa657f281
geebzter <geebzter@gmail.com>
2021-02-10 16:19:32 -0500

    Sixth version: Upgraded schema to incorporate all fields from csv file Updated resetmigration script to import data post migration reset

--- recommender/models.py
+++ recommender/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 
 class Musicdata(models.Model):
-    id = models.TextField(primary_key=True)
     valence = models.FloatField()
     year = models.IntegerField()
     acousticness = models.FloatField()
-    danceability = models.FloatField()
-    energy = models.FloatField()
-    popularity = models.FloatField()
     artists = models.TextField()
+    danceability = models.FloatField()
+    duration_ms = models.FloatField()
+    energy = models.FloatField()
+    explicit = models.FloatField()
+    id = models.TextField(primary_key=True)
+    instrumentalness = models.FloatField()
+    key = models.FloatField()
+    liveness = models.FloatField()
+    loudness = models.FloatField()
+    mode = models.FloatField()
     name = models.TextField()
+    popularity = models.FloatField()
+    release_date = models.IntegerField()
+    speechiness = models.FloatField()
+    tempo = models.FloatField()
 
-    class Meta:
-        managed = False
-        db_table = 'musicdata'
+
